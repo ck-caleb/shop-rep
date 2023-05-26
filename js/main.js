d0f3c8cfcd231b3e5e83770599465724b1f836a6
caleb <mdaicaleb@gmail.com>
2023-05-25 20:49:13 -0700

    refactored files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const customerReviewContentText = document.getElementById(
+  "customer-review-text"
+);
+const customerReviewButton = document.getElementById("customer-review-button");
+customerReviewButton.onclick = function () {
+  if (customerReviewContentText.className == "open") {
+    //shrink the text
+    // customerReviewContentText.className = "";
+    customerReviewContentText.className = "";
 
+    customerReviewButton.innerHTML = "show more";
+  } else {
+    //expand the text
+    customerReviewContentText.className = "open";
+    customerReviewButton.innerHTML = "show less";
+  }
+};
 
- 
